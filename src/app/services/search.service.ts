9b16147181783a2d8bdafd1f9c1df56c7c8a2039
Victor Nascimento <victormota15@gmail.com>
2018-06-04 19:15:27 -0300

    Extract API url

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const API_URL = 'http://localhost:3000'
+
 @Injectable()
 export class SearchService {
 
   constructor(private http :  HttpClient) { }
 
   public search(term: string) {
-    return this.http.get(`http://localhost:3000/search?term=${term}`).toPromise()
+    return this.http.get(`${API_URL}/search?term=${term}`).toPromise()
   }
 
 }
