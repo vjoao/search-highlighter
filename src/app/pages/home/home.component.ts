bf74ed8755582c4bdb1cfd02d79c50d505582dda
Victor Nascimento <victormota15@gmail.com>
2018-06-04 21:14:28 -0300

    Highlight logic impl

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../../services/search.service'
-
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,15 +9,38 @@ import { SearchService } from '../../services/search.service'
 export class HomeComponent implements OnInit {
 
   private term : string;
+  private result : string;
+  private highlighted: SafeHtml;
 
-  constructor(private searchService: SearchService) { }
+  constructor(private searchService: SearchService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
 
+  generateHighlights (type, results) {
+    const { brands : brandCounts, clothes: clothCounts } = results
+
+    const merged = brandCounts.map((brandMatch, index) => {
+      const clothMatch = clothCounts[index]
+
+      if(brandMatch.count === 0 && clothMatch.count === 0) {
+        return brandMatch.brand
+      } else if(brandMatch.count > clothMatch.count) {
+        return `<b>${brandMatch.brand}</b>`
+      } else if (brandMatch.count < clothMatch.count) {
+        return `<em>${brandMatch.brand}</em>`
+      }
+
+      return `<b><em>${brandMatch.brand}</em></b>`
+    })
+
+    this.highlighted = this.sanitizer.bypassSecurityTrustHtml(merged.join(' '))
+  }
+
   performSearch (term) {
     this.searchService.search(term).then(results => {
-      console.log(results)
+      this.result = results.toString()
+      this.generateHighlights('brands', results)
     })
   }
 
